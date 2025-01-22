87c4465ef5d8dad3f191d9b0f03c4bf36ab38983
kavinpradheep <kavinpradheep@gmail.com>
2025-01-22 13:14:26 +0530

    before backend

---
+++ admindata.js
@@ -0,0 +1,17 @@
+// adminCredentials.js
+const adminCredentials = [
+    { email: "admin1@example.com", password: "password123" },
+    { email: "admin2@example.com", password: "password123" },
+    { email: "admin3@example.com", password: "password123" },
+    { email: "admin4@example.com", password: "password123" },
+    { email: "admin5@example.com", password: "password123" },
+    { email: "admin6@example.com", password: "password123" },
+    { email: "admin7@example.com", password: "password123" },
+    { email: "admin8@example.com", password: "password123" },
+    { email: "admin9@example.com", password: "password123" },
+    { email: "admin10@example.com", password: "password123" },
+  ];
+
+  
+  export default adminCredentials;
+  
---
+++ colleges.js
@@ -0,0 +1,10 @@
+// colleges.js
+const colleges = [
+    "College of Engineering",
+    "Business School",
+    "Medical College",
+    "Arts and Science College",
+    "Law School",
+];
+
+export default colleges;
